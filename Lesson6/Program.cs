7424bff135458ab57f1a4b705eea15e3c7dd4d35
ElkhanTaghizade <elxantagizad@gmail.com>
2023-08-17 01:54:37 +0400

    Task

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -13,20 +13,37 @@ namespace CSharp_Task5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("                                   Shopping List");
-            ArrayList shoppinglist = new ArrayList();
+            Console.WriteLine("                                                       Shopping List");
+            ArrayList categories = new ArrayList();
             var shoppinglistin1 = new
             {
+                Category="Category1",
                 Item="Bread",
                 Amount=2,
             };
             var shoppinglistin2 = new
             {
+                Category="Category1",
                 Item = "Egg",
                 Amount = 20,
             };
-            shoppinglist.Add(shoppinglistin1);
-            shoppinglist.Add(shoppinglistin2);
+            var shoppinglist11 = new
+            {
+                Category = "Category2",
+                Item = "Apple",
+                Amount = 8
+            };
+            var shoppinglist22 = new
+            {
+                Category = "Category2",
+                Item = "Orange",
+                Amount = 6
+            };
+            categories.Add(shoppinglistin1);
+            categories.Add(shoppinglistin2);
+            categories.Add(shoppinglist11);
+            categories.Add(shoppinglist22);
+
 
             bool check = true;
             while (check)    
@@ -39,43 +56,54 @@ namespace CSharp_Task5
                 string choose=Console.ReadLine();
               
                 switch (choose)
-                    {
+                {
                         case "1":
 
                             Console.WriteLine("Enter the number of the item you want to add:");
-                            byte itemnum = Convert.ToByte(Console.ReadLine());
+                            byte itemnumadd = Convert.ToByte(Console.ReadLine());
 
-                            for (int i = 0; i < itemnum; i++)
+                        for (int i = 0; i < itemnumadd; i++)
+                        {
+                            Console.WriteLine("Enter the category you want to add:");
+                            string categoryadd=Console.ReadLine();
+                            Console.WriteLine("Enter the item you want to add:");
+                            string itemadd = Console.ReadLine();
+                            Console.WriteLine("Enter the amount of the item you want to add");
+                            int itemamountadd = Convert.ToInt32(Console.ReadLine());
+
+                            var add = new
                             {
+                                category= categoryadd,
+                                Item = itemadd,
+                                Amount = itemamountadd
+                            };
+                            categories.Add(add);
 
-                                Console.WriteLine("Enter the item you want to add");
-                                string item = Console.ReadLine();
-                                Console.WriteLine("Enter the amount of the item you want to add");
-                                int itemamount = Convert.ToInt32(Console.ReadLine());
-
-                                var shoppingadd = new
-                                {
-                                    Item = item,
-                                    Amount = itemamount
-                                };
-                                shoppinglist.Add(shoppingadd);
-
-                            }
-                            Console.WriteLine("Items added.");
-                            Console.WriteLine("");
-                            foreach (var items2 in shoppinglist)
-                            {
-                                Console.WriteLine(items2);
-                            }
-
-                            break;
+                        }
+                        Console.WriteLine("Items added.");
+                     
+                        break;
 
                         case "2":
+                        foreach(dynamic item in categories)
+                        {
+                            Console.WriteLine(item.Category);
+                        }
+                          Console.WriteLine("Enter the category you want to display:");
+                          string checkcategory=Console.ReadLine();
 
-                            foreach (object items in shoppinglist)
+                        foreach(dynamic item in categories)
+                        {
+                            if(item.Category == checkcategory)
                             {
-                                Console.WriteLine(items);
+                                Console.WriteLine(item.Item);
                             }
+                            else
+                            {
+                                Console.WriteLine("Category not found");
+                            }
+
+                        }
 
                             break;
 
@@ -91,7 +119,7 @@ namespace CSharp_Task5
                             Console.WriteLine("Wrong button.");
 
                             break;
-                    }
+                }
                 
             }
 
