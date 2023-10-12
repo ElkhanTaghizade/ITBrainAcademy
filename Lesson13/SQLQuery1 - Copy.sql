ecc11355aba02eaae924b9e79135e8f82f266fed
ElkhanTaghizade <elxantagizad@gmail.com>
2023-10-12 17:16:15 +0400

    SQL Task1

---
+++ Lesson13/SQLQuery1 - Copy.sql
@@ -0,0 +1,24 @@
+--create database Academy
+--use Academy
+--create table Student
+--( grade nvarchar(10),
+--fullname nvarchar(30),
+--birthdate date,
+--groupNo nvarchar (8)
+--)
+--Insert into Student (fullname, grade, birthdate, groupNo)
+--values
+--( 'Elkhan', 'A', '01-01-1500','P114'),
+--( 'Elkhan2', 'B', '01-01-1499','P114')
+-- delete from Student
+ --where fullname='Elkhan2' 
+ --delete from Student
+ --where fullname='Elkhan2'
+-- update Student
+--set fullname='Elkhan111',
+-- grade='C',
+-- birthdate='02-02-2022',
+-- groupNo='P839398'
+--where fullname='Elkhan'
+--select * from Student
+--where fullname='Elkkhan'
